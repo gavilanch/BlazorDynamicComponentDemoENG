5f6551426d28d6dbcda65e169fada8b23a7f449d
Felipe Gavilan <elmejor_887@hotmail.com>
2021-04-30 12:03:44 -0400

    Initial commit

---
+++ Blazor6/Client/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor6.Client
+{
+    public class AppState
+    {
+        public Action Action { get; set; }
+        public Action<Dictionary<string, object>> ActionWithParameters { get; set; }
+    }
+}
---
+++ Blazor6/Client/ComponenteJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor6.Client
+{
+
+    public class JSONComponent
+    {
+        public string Component { get; set; }
+        public IDictionary<string, object> Parameters { get; set; }
+    }
+}
